e98ac7fe8934826201b6c63657b139e7df51f827
joeyarbrough <jcyarbro88@gmail.com>
2020-12-29 16:32:50 -0600

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,6 @@ const styles: Styles<Theme, StyledComponentProps> = (theme: Theme) => ({
   root: {
     display: 'flex',
   },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  fullWidth: {
-    width: '100%',
-  },
 });
 
 class App extends Component<AppProps> {
