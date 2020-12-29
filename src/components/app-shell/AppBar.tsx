cfc5ccde25a7cf45d151f7dbcaf2916100d69b63
joeyarbrough <jcyarbro88@gmail.com>
2020-12-29 17:05:33 -0600

    simplifying appbar

--- src/components/app-shell/AppBar.tsx
+++ src/components/app-shell/AppBar.tsx
@@ -51,37 +51,7 @@ export default function MenuAppBar() {
         <Typography variant={'h6'} className={classes.title}>
           {"Jamie's Teacher App"}
         </Typography>
-        {auth && (
-          <div>
-            <IconButton
-              aria-label={'account of current user'}
-              aria-controls={'menu-appbar'}
-              aria-haspopup={'true'}
-              onClick={handleMenu}
-              color={'inherit'}
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id={'menu-appbar'}
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={open}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>Profile</MenuItem>
-              <MenuItem onClick={handleClose}>My account</MenuItem>
-            </Menu>
-          </div>
-        )}
+        <AccountCircle />
       </Toolbar>
     </AppBar>
   );
