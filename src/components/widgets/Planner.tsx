55255d8b8502203e7be99b1290a3d917b5bd0efa
joeyarbrough <jcyarbro88@gmail.com>
2020-12-29 21:01:23 -0600

    using demo

---
+++ src/components/widgets/Planner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Demo from './planner-calendar/demo';
+
+class Planner extends React.Component {
+  render() {
+    return <Demo />;
+  }
+}
+
+export default Planner;
