593c0be66f62451f7a3f31ea33e41c5a9ae7e817
joeyarbrough <jcyarbro88@gmail.com>
2021-01-02 16:17:41 -0600

    ripping stuff out

--- src/components/widgets/demo/DndDemo.tsx
+++ src/components/widgets/demo/DndDemo.tsx
@@ -11,61 +11,13 @@ import {
   DraggableStateSnapshot,
 } from 'react-beautiful-dnd';
 import { Card, Grid } from '@material-ui/core';
-
-interface Item {
-  id: string;
-  content: string;
-}
+import { Item } from '../../../configs/types/Item';
+import { IMoveResult } from '../../../configs/types/IMoveResult';
+import { getItems, move, reorder } from '../../../utils/weekly-schedule';
 interface IAppState {
   [key: string]: Item[];
   selected: Item[];
 }
-
-interface IMoveResult {
-  droppable: Item[];
-  droppable2: Item[];
-}
-
-const getItems = (count: number, offset = 0): Item[] => {
-  return Array.from({ length: count }, (v, k) => k).map((k) => ({
-    content: `item ${k + offset}`,
-    id: `item-${k + offset}`,
-  }));
-};
-
-const reorder = (
-  list: Item[],
-  startIndex: number,
-  endIndex: number
-): Item[] => {
-  const result = [...list];
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
-
-/**
- * Moves an item from one list to another list.
- */
-const move = (
-  source: Item[],
-  destination: Item[],
-  droppableSource: DraggableLocation,
-  droppableDestination: DraggableLocation
-): IMoveResult | any => {
-  const sourceClone = [...source];
-  const destClone = [...destination];
-  const [removed] = sourceClone.splice(droppableSource.index, 1);
-
-  destClone.splice(droppableDestination.index, 0, removed);
-
-  const result: any = {};
-  result[droppableSource.droppableId] = sourceClone;
-  result[droppableDestination.droppableId] = destClone;
-
-  return result;
-};
 
 const grid = 8;
 
