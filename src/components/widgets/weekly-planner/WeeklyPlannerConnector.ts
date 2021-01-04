aeecb40951c710190717fdd110607f18775ccc06
joeyarbrough <jcyarbro88@gmail.com>
2021-01-03 20:52:47 -0600

    using handler

--- src/components/widgets/weekly-planner/WeeklyPlannerConnector.ts
+++ src/components/widgets/weekly-planner/WeeklyPlannerConnector.ts
@@ -1,6 +1,9 @@
 import { connect } from 'react-redux';
 import { State } from '../../../configs/redux/store';
 import WeeklyPlanner, { WeeklyPlannerProps } from './WeeklyPlanner';
+import { Dispatch } from 'redux';
+import { PlannerItem } from '../../../configs/types/WeeklyPlanner';
+import { reorderPlannerItems } from '../../../creators/weekly-planner';
 
 const mapStateToProps = (state: State): WeeklyPlannerProps => {
   const selectedPlanner = state.applicationState.weeklyPlanners.find(
@@ -14,7 +17,11 @@ const mapStateToProps = (state: State): WeeklyPlannerProps => {
   } as unknown) as WeeklyPlannerProps;
 };
 
-const mapDispatchToProps = (): WeeklyPlannerProps =>
-  (({} as unknown) as WeeklyPlannerProps);
+const mapDispatchToProps = (dispatch: Dispatch): WeeklyPlannerProps =>
+  (({
+    reorderHandler: (items: PlannerItem[], dayOfWeek: string) => {
+      dispatch(reorderPlannerItems(items, dayOfWeek));
+    },
+  } as unknown) as WeeklyPlannerProps);
 
 export default connect(mapStateToProps, mapDispatchToProps)(WeeklyPlanner);
