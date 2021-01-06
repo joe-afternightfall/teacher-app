66580e709f6161e09ae8913aaf9887fa0bd52104
jyarbrough <jcyarbro88@gmail.com>
2021-01-06 08:14:03 -0600

    adding mock-redux

--- src/components/top-level-components/DashboardScreen.test.tsx
+++ src/components/top-level-components/DashboardScreen.test.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import DashboardScreen from './DashboardScreen';
-import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { getStore, renderWithRedux } from '../../configs/test-utils/mock-redux';
 
 describe('Dashboard Screen', () => {
   it('should render with default title', () => {
-    const dashboard = render(<DashboardScreen />);
+    const dashboard = renderWithRedux(<DashboardScreen />, getStore({}, null));
 
-    expect(dashboard.getByText('TEACHER-APP')).toBeInTheDocument();
+    expect(dashboard.getByText('Monday')).toBeInTheDocument();
   });
 });
---
+++ src/configs/test-utils/mock-redux.tsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import { Store } from 'redux';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import createStore, { MockStore } from 'redux-mock-store';
+import { createStore as createRealStore } from '../../configs/redux/store';
+import { createHashHistory, History } from 'history';
+import thunk from 'redux-thunk';
+
+const middleware = [thunk];
+
+export function renderWithRedux(ui: JSX.Element, store: Store) {
+  return render(<Provider store={store}>{ui}</Provider>);
+}
+
+export async function renderAsyncWithRedux(ui: JSX.Element, store: any) {
+  // eslint-disable-next-line no-undef
+  return new Promise((resolve, reject) => {
+    const result = render(<Provider store={store}>{ui}</Provider>);
+    setTimeout(() => {
+      resolve(result);
+    }, 100);
+  });
+}
+
+const weeklyPlanners = [buildWeeklyPlanner()];
+
+export const initialState = {
+  applicationState: {
+    weeklyPlanners: weeklyPlanners,
+    selectedPlannerId: weeklyPlanners[0].id,
+  },
+};
+
+export function getStore(state: object, dispatchMock: any): MockStore {
+  const store = createStore(middleware)({
+    ...initialState,
+    applicationState: { ...initialState.applicationState, ...state },
+  });
+
+  if (dispatchMock) {
+    return { ...store, dispatch: dispatchMock };
+  } else {
+    return store;
+  }
+}
+
+// export function getInboxStore(state: object, dispatchMock: any): MockStore {
+//   let store = createStore(middleware)({
+//     ...initialState,
+//     auditInboxState: {...initialState.auditInboxState, ...state},
+//   });
+//   if (dispatchMock) {
+//     return {...store, dispatch: dispatchMock};
+//   } else {
+//     return store;
+//   }
+// }
+
+// export function getFullStore(state: object, dispatchMock: any): MockStore {
+//   let store = createStore(middleware)({
+//     ...initialState,
+//     applicationState: {...initialState.applicationState, ...state},
+//     auditInboxState: {...initialState.auditInboxState, ...state},
+//   });
+//   if (dispatchMock) {
+//     return {...store, dispatch: dispatchMock};
+//   } else {
+//     return store;
+//   }
+// }
+
+export function getRealStore() {
+  const history: History = createHashHistory();
+  return createRealStore(history);
+}
+
+export function buildPlannerItem(items: number) {
+  let index = 0;
+  const builtList = [];
+
+  while (index < items) {
+    index += 1;
+
+    builtList.push({
+      id: `test-id-${index}`,
+      content: `test-content-${index}`,
+    });
+  }
+
+  return builtList;
+}
+
+export function buildWeeklyPlanner() {
+  return {
+    createdAt: '123456789',
+    id: 'planner-id',
+    title: 'planner-title',
+    items: {
+      monday: {
+        date: '01/01/2021',
+        items: buildPlannerItem(3),
+      },
+      tuesday: {
+        date: '01/02/2021',
+        items: buildPlannerItem(1),
+      },
+      wednesday: {
+        date: '01/03/2021',
+        items: buildPlannerItem(2),
+      },
+      thursday: {
+        date: '01/04/2021',
+        items: buildPlannerItem(1),
+      },
+      friday: {
+        date: '01/05/2021',
+        items: buildPlannerItem(2),
+      },
+    },
+    notes: [
+      {
+        id: 'note-id-one',
+        content: 'note-content-one',
+      },
+      {
+        id: 'note-id-two',
+        content: 'note-content-two',
+      },
+    ],
+  };
+}
