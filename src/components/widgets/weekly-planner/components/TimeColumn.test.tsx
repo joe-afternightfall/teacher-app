5ef639b0da20bd4b162b2183fe756bcb5d46b5f8
jyarbrough <jcyarbro88@gmail.com>
2021-01-06 08:13:15 -0600

    adding tests

---
+++ src/components/widgets/weekly-planner/components/TimeColumn.test.tsx
@@ -0,0 +1,18 @@
+import TimeColumn from './TimeColumn';
+import { render } from '@testing-library/react';
+import * as React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Time Column Component', () => {
+  it('should render with values', () => {
+    const timeColumn = render(<TimeColumn />);
+
+    expect(timeColumn.getByText('8:30 - 9:00 am')).toBeInTheDocument();
+    expect(timeColumn.getByText('9:00 - 9:30 am')).toBeInTheDocument();
+    expect(timeColumn.getByText('9:30 - 10:00 am')).toBeInTheDocument();
+    expect(timeColumn.getByText('10:00 - 10:30 am')).toBeInTheDocument();
+    expect(timeColumn.getByText('10:30 - 11:00 am')).toBeInTheDocument();
+    expect(timeColumn.getByText('11:00 - 11:30 am')).toBeInTheDocument();
+    expect(timeColumn.getByText('11:30 - 12:00 pm')).toBeInTheDocument();
+  });
+});
