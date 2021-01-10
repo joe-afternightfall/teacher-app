1ff02422c95d63360ed723483f8fa5658afe111d
jyarbrough <jcyarbro88@gmail.com>
2021-01-09 22:10:51 -0600

    cleanup app reducer

--- src/reducers/application.ts
+++ src/reducers/application.ts
@@ -1,6 +1,5 @@
 import { AnyAction } from 'redux';
 import actions from '../creators/actions';
-import { Planner } from '../configs/types/WeeklyPlanner';
 
 export default {
   reducer(
@@ -11,36 +10,6 @@ export default {
 
     switch (action.type) {
       case actions.INITIALIZE:
-        newState.weeklyPlanners = action.weeklyPlanners;
-        newState.selectedPlannerId = action.weeklyPlanners[0].id;
-        break;
-      case actions.REORDER_WEEKLY_PLANNER: {
-        const selectedPlanner = newState.weeklyPlanners.find((planner) => {
-          return planner.id === newState.selectedPlannerId;
-        });
-
-        if (selectedPlanner !== undefined) {
-          selectedPlanner.items[action.dayOfWeek].items = action.items;
-        }
-        break;
-      }
-      case actions.MOVE_PLANNER_ITEMS: {
-        const selectedPlanner = newState.weeklyPlanners.find((planner) => {
-          return planner.id === newState.selectedPlannerId;
-        });
-
-        if (selectedPlanner !== undefined) {
-          selectedPlanner.items.monday.items = action.items.monday;
-          selectedPlanner.items.tuesday.items = action.items.tuesday;
-          selectedPlanner.items.wednesday.items = action.items.wednesday;
-          selectedPlanner.items.thursday.items = action.items.thursday;
-          selectedPlanner.items.friday.items = action.items.friday;
-        }
-        break;
-      }
-      case actions.LOAD_WEEKLY_PLANNERS:
-        newState.weeklyPlanners = action.weeklyPlanners;
-        newState.selectedPlannerId = action.weeklyPlanners[0].id;
         break;
       default:
         newState = state;
@@ -52,7 +21,5 @@ export default {
 
 export interface ApplicationState {
   username: string;
-  selectedPlannerId: string;
   displayEditingForm: boolean;
-  weeklyPlanners: Planner[];
 }
