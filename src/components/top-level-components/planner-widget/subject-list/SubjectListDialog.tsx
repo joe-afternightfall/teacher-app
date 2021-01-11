8d6fbae07ef2a9996fe56fdd7055f4d3eafd4fa1
jyarbrough <jcyarbro88@gmail.com>
2021-01-10 22:02:10 -0600

    ripping out color selector

--- src/components/top-level-components/planner-widget/subject-list/SubjectListDialog.tsx
+++ src/components/top-level-components/planner-widget/subject-list/SubjectListDialog.tsx
@@ -1,46 +1,36 @@
 import React, { Component } from 'react';
 import { Styles } from '@material-ui/styles';
-import clsx from 'clsx';
 import {
-  StyledComponentProps,
   Theme,
   WithStyles,
   withStyles,
+  StyledComponentProps,
 } from '@material-ui/core/styles';
 import {
   Grid,
-  Card,
-  Avatar,
-  Button,
-  Dialog,
-  IconButton,
-  CardHeader,
-  CardContent,
-  DialogTitle,
-  DialogContent,
   List,
   Paper,
-  Tooltip,
+  Button,
+  Dialog,
   ListItem,
   MenuItem,
   TextField,
+  IconButton,
   Typography,
+  DialogTitle,
   ListItemText,
+  DialogContent,
   DialogActions,
   SvgIconTypeMap,
   InputAdornment,
   ListItemSecondaryAction,
 } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import CheckIcon from '@material-ui/icons/Check';
-import { Subject } from '../../configs/types/WeeklyPlanner';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import { subjectIcons } from '../../configs/theme/subject-icon-choices';
 import { OverridableComponent } from '@material-ui/core/OverridableComponent';
-import { subjectColorChoices } from '../../configs/theme/subject-color-choices';
-import AddIcon from '@material-ui/icons/Add';
-import PreviewCard from './planner-widget/subject-info/PreviewCard';
-import ColorChoices from './planner-widget/subject-info/ColorChoices';
+import { Subject } from '../../../../configs/types/WeeklyPlanner';
+import { subjectIcons } from '../../../../configs/theme/subject-icon-choices';
+import ColorSelector from './subject-info/color-selector/ColorSelector';
 
 const styles: Styles<Theme, StyledComponentProps> = (theme: Theme) => ({
   closeButton: {
@@ -224,25 +214,14 @@ class SubjectListDialog extends Component<SubjectListDialogProps> {
                   </Grid>
                 </Grid>
 
-                <Grid item xs={12} container spacing={2}>
-                  <Grid item xs={12}>
-                    <Typography variant={'h6'}>
-                      {`Subject Color: ${this.state.colorName}`}
-                    </Typography>
-                  </Grid>
-
-                  <ColorChoices
-                    colorName={this.state.colorName}
-                    selectClickHandler={selectColor}
-                  />
-
-                  <PreviewCard
-                    color={this.state.color}
-                    secondaryColor={this.state.secondaryColor}
-                    selectedIcon={this.state.selectedIcon}
-                    subjectName={this.state.subjectName}
-                  />
-                </Grid>
+                <ColorSelector
+                  colorName={this.state.colorName}
+                  color={this.state.color}
+                  secondaryColor={this.state.secondaryColor}
+                  selectedIcon={this.state.selectedIcon}
+                  subjectName={this.state.subjectName}
+                  selectClickHandler={selectColor}
+                />
               </Grid>
             </Grid>
           </DialogContent>
