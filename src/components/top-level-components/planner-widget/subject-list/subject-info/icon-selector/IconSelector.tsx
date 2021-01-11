a39ea63996b2aec1c4f61c5912d7fcc1c6285494
jyarbrough <jcyarbro88@gmail.com>
2021-01-11 11:28:54 -0600

    cleanup

--- src/components/top-level-components/planner-widget/subject-list/subject-info/icon-selector/IconSelector.tsx
+++ src/components/top-level-components/planner-widget/subject-list/subject-info/icon-selector/IconSelector.tsx
@@ -1,85 +1,84 @@
-import {
-  Theme,
-  WithStyles,
-  withStyles,
-  StyledComponentProps,
-} from '@material-ui/core/styles';
-import React, { Component } from 'react';
-import { Styles } from '@material-ui/styles';
+import React from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
 import {
   SubjectIcon,
   subjectIcons,
 } from '../../../../../../configs/theme/subject-icon-choices';
+import { State } from '../../../../../../configs/redux/store';
+import { selectIcon } from '../../../../../../creators/subject-list';
 import { Grid, Paper, SvgIconTypeMap, Typography } from '@material-ui/core';
 import { OverridableComponent } from '@material-ui/core/OverridableComponent';
 
-const styles: Styles<Theme, StyledComponentProps> = () => ({});
+const IconSelector = (props: IconSelectorProps) => {
+  const [isHovering, setIsHovering] = React.useState<string>('');
 
-class IconSelector extends Component<IconSelectorProps> {
-  state = {
-    isHovering: '',
+  const handleHover = (id: string) => (event: any) => {
+    setIsHovering(id);
   };
 
-  render(): JSX.Element {
-    const { selectIconHandler, selectedIcon } = this.props;
-
-    const handleHover = (id: string) => (event: any) => {
-      this.setState({
-        isHovering: id,
-      });
-    };
-
-    return (
-      <Grid item xs={7} container>
-        <Grid item xs={12}>
-          <Typography variant={'h6'}>{'Subject Icon'}</Typography>
-        </Grid>
-        <Grid item xs={12} container>
-          {subjectIcons.map((icon: SubjectIcon, index) => {
-            return (
-              <Grid
-                item
-                xs={2}
-                key={index}
-                style={{
-                  textAlign: 'center',
-                }}
-                onMouseLeave={handleHover('')}
-                onMouseEnter={handleHover(icon.id)}
-              >
-                {this.state.isHovering === icon.id ? (
-                  <Paper
-                    elevation={3}
-                    style={{
-                      width: '100%',
-                      // textAlign: 'center',
-                      cursor: 'pointer',
-                    }}
-                    onClick={() => {
-                      selectIconHandler(icon.icon);
-                    }}
-                  >
-                    <Grid container alignItems={'center'} justify={'center'}>
-                      <Grid item>{React.createElement(icon.icon)}</Grid>
-                    </Grid>
-                  </Paper>
-                ) : selectedIcon === icon.icon ? (
-                  <Paper elevation={3}>{React.createElement(icon.icon)}</Paper>
-                ) : (
-                  React.createElement(icon.icon)
-                )}
-              </Grid>
-            );
-          })}
-        </Grid>
+  return (
+    <Grid item xs={7} container>
+      <Grid item xs={12}>
+        <Typography variant={'h6'}>{'Subject Icon'}</Typography>
       </Grid>
-    );
-  }
-}
+      <Grid item xs={12} container>
+        {subjectIcons.map((icon: SubjectIcon, index) => {
+          return (
+            <Grid
+              item
+              xs={2}
+              key={index}
+              style={{
+                textAlign: 'center',
+              }}
+              onMouseLeave={handleHover('')}
+              onMouseEnter={handleHover(icon.id)}
+            >
+              {isHovering === icon.id ? (
+                <Paper
+                  elevation={3}
+                  style={{
+                    width: '100%',
+                    cursor: 'pointer',
+                  }}
+                  onClick={() => {
+                    props.selectIconHandler(icon.icon);
+                  }}
+                >
+                  <Grid container alignItems={'center'} justify={'center'}>
+                    <Grid item>{React.createElement(icon.icon)}</Grid>
+                  </Grid>
+                </Paper>
+              ) : props.selectedIcon === icon.icon ? (
+                <Paper elevation={3}>{React.createElement(icon.icon)}</Paper>
+              ) : (
+                React.createElement(icon.icon)
+              )}
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Grid>
+  );
+};
 
-export interface IconSelectorProps extends WithStyles<typeof styles> {
+export interface IconSelectorProps {
   selectedIcon: OverridableComponent<SvgIconTypeMap>;
   selectIconHandler: (icon: OverridableComponent<SvgIconTypeMap>) => void;
 }
 
-export default withStyles(styles, { withTheme: true })(IconSelector);
+const mapStateToProps = (state: State): IconSelectorProps => {
+  return ({
+    selectedIcon: state.subjectListState.selectedIcon,
+  } as unknown) as IconSelectorProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): IconSelectorProps =>
+  (({
+    selectIconHandler: (icon: OverridableComponent<SvgIconTypeMap>) => {
+      dispatch(selectIcon(icon));
+    },
+  } as unknown) as IconSelectorProps);
+
+export default connect(mapStateToProps, mapDispatchToProps)(IconSelector);
