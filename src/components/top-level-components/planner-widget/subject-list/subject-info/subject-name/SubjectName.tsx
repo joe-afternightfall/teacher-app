fccd2e6cd29f6dbd290baffa61d07605904c8493
jyarbrough <jcyarbro88@gmail.com>
2021-01-11 11:31:49 -0600

    cleanup

--- src/components/top-level-components/planner-widget/subject-list/subject-info/subject-name/SubjectName.tsx
+++ src/components/top-level-components/planner-widget/subject-list/subject-info/subject-name/SubjectName.tsx
@@ -1,11 +1,4 @@
-import React, { Component } from 'react';
-import { Styles } from '@material-ui/styles';
-import {
-  StyledComponentProps,
-  Theme,
-  WithStyles,
-  withStyles,
-} from '@material-ui/core/styles';
+import React from 'react';
 import {
   Grid,
   InputAdornment,
@@ -13,45 +6,59 @@ import {
   TextField,
   Typography,
 } from '@material-ui/core';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { State } from '../../../../../../configs/redux/store';
+import { updateSubjectName } from '../../../../../../creators/subject-list';
 import { OverridableComponent } from '@material-ui/core/OverridableComponent';
 
-const styles: Styles<Theme, StyledComponentProps> = () => ({});
-
-class SubjectName extends Component<SubjectNameProps> {
-  render(): JSX.Element {
-    const { subjectName, handleTextChange, selectedIcon } = this.props;
-
-    return (
-      <Grid item xs={5} container>
-        <Grid item xs={12}>
-          <Typography variant={'h6'}>{'Subject Name'}</Typography>
-        </Grid>
-
-        <Grid item xs={12} style={{ textAlign: 'center' }}>
-          <TextField
-            autoFocus
-            value={subjectName}
-            onChange={handleTextChange}
-            variant={'outlined'}
-            style={{ width: '70%' }}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position={'start'}>
-                  {React.createElement(selectedIcon)}
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
+const SubjectName = (props: SubjectNameProps) => {
+  return (
+    <Grid item xs={5} container>
+      <Grid item xs={12}>
+        <Typography variant={'h6'}>{'Subject Name'}</Typography>
       </Grid>
-    );
-  }
-}
 
-export interface SubjectNameProps extends WithStyles<typeof styles> {
+      <Grid item xs={12} style={{ textAlign: 'center' }}>
+        <TextField
+          autoFocus
+          value={props.subjectName}
+          onChange={props.updateSubjectNameHandler}
+          variant={'outlined'}
+          style={{ width: '70%' }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position={'start'}>
+                {React.createElement(props.selectedIcon)}
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export interface SubjectNameProps {
   subjectName: string;
   selectedIcon: OverridableComponent<SvgIconTypeMap>;
-  handleTextChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  updateSubjectNameHandler: (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => void;
 }
 
-export default withStyles(styles, { withTheme: true })(SubjectName);
+const mapStateToProps = (state: State): SubjectNameProps => {
+  return ({
+    subjectName: state.subjectListState.subjectName,
+    selectedIcon: state.subjectListState.selectedIcon,
+  } as unknown) as SubjectNameProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): SubjectNameProps =>
+  (({
+    updateSubjectNameHandler: (event: React.ChangeEvent<HTMLInputElement>) => {
+      dispatch(updateSubjectName(event.target.value));
+    },
+  } as unknown) as SubjectNameProps);
+
+export default connect(mapStateToProps, mapDispatchToProps)(SubjectName);
