fdd1f47644d5c7371bc160888de739c49aad21ac
jyarbrough <jcyarbro88@gmail.com>
2021-01-11 11:29:29 -0600

    cleanup

--- src/components/top-level-components/planner-widget/subject-list/subject-info/color-selector/components/PreviewCard.tsx
+++ src/components/top-level-components/planner-widget/subject-list/subject-info/color-selector/components/PreviewCard.tsx
@@ -1,11 +1,4 @@
-import {
-  Theme,
-  WithStyles,
-  withStyles,
-  StyledComponentProps,
-} from '@material-ui/core/styles';
-import React, { Component } from 'react';
-import { Styles } from '@material-ui/styles';
+import React from 'react';
 import {
   Card,
   Grid,
@@ -14,47 +7,45 @@ import {
   CardContent,
   SvgIconTypeMap,
 } from '@material-ui/core';
+import { connect } from 'react-redux';
+import { State } from '../../../../../../../configs/redux/store';
 import { OverridableComponent } from '@material-ui/core/OverridableComponent';
+import { ColorChoice } from '../../../../../../../configs/theme/subject-color-choices';
 
-const styles: Styles<Theme, StyledComponentProps> = () => ({});
+const PreviewCard = (props: PreviewCardProps) => {
+  return (
+    <Grid item xs={7}>
+      <Card style={{ width: '75%', height: 200, margin: 'auto' }}>
+        <CardHeader
+          title={`Sample ${props.subjectName} Card`}
+          style={{
+            backgroundColor: props.selectedColor.primaryColor,
+            color: props.selectedColor.secondaryColor,
+          }}
+          avatar={
+            <Avatar aria-label={'subject-icon'}>
+              {React.createElement(props.selectedIcon)}
+            </Avatar>
+          }
+        />
+        <CardContent>{'Sample Card Content'}</CardContent>
+      </Card>
+    </Grid>
+  );
+};
 
-class PreviewCard extends Component<PreviewCardProps> {
-  render(): JSX.Element {
-    const {
-      color,
-      classes,
-      subjectName,
-      selectedIcon,
-      secondaryColor,
-    } = this.props;
-
-    return (
-      <Grid item xs={7}>
-        <Card style={{ width: '75%', height: 200, margin: 'auto' }}>
-          <CardHeader
-            title={`Sample ${subjectName} Card`}
-            style={{
-              backgroundColor: color,
-              color: secondaryColor,
-            }}
-            avatar={
-              <Avatar aria-label={'recipe'} className={classes.avatar}>
-                {React.createElement(selectedIcon)}
-              </Avatar>
-            }
-          />
-          <CardContent>{'Sample Card Content'}</CardContent>
-        </Card>
-      </Grid>
-    );
-  }
-}
-
-export interface PreviewCardProps extends WithStyles<typeof styles> {
-  color: string;
-  secondaryColor: string;
+export interface PreviewCardProps {
   subjectName: string;
+  selectedColor: ColorChoice;
   selectedIcon: OverridableComponent<SvgIconTypeMap>;
 }
 
-export default withStyles(styles, { withTheme: true })(PreviewCard);
+const mapStateToProps = (state: State): PreviewCardProps => {
+  return ({
+    subjectName: state.subjectListState.subjectName,
+    selectedColor: state.subjectListState.selectedColor,
+    selectedIcon: state.subjectListState.selectedIcon,
+  } as unknown) as PreviewCardProps;
+};
+
+export default connect(mapStateToProps)(PreviewCard);
