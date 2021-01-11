fed01808a98b4e862b4c2c09b8e495c9f425d412
jyarbrough <jcyarbro88@gmail.com>
2021-01-11 11:28:35 -0600

    simplify

--- src/components/top-level-components/planner-widget/subject-list/subject-info/color-selector/ColorSelector.tsx
+++ src/components/top-level-components/planner-widget/subject-list/subject-info/color-selector/ColorSelector.tsx
@@ -1,50 +1,38 @@
-import {
-  Theme,
-  WithStyles,
-  withStyles,
-  StyledComponentProps,
-} from '@material-ui/core/styles';
-import React, { Component } from 'react';
-import { Styles } from '@material-ui/styles';
+import React from 'react';
+import { connect } from 'react-redux';
 import PreviewCard from './components/PreviewCard';
-import ColorChoices from './components/ColorChoices';
-import { Grid, SvgIconTypeMap, Typography } from '@material-ui/core';
-import { OverridableComponent } from '@material-ui/core/OverridableComponent';
-const styles: Styles<Theme, StyledComponentProps> = () => ({});
+import { Grid, Typography } from '@material-ui/core';
+import { State } from '../../../../../../configs/redux/store';
+import ColorChoices, { ColorChoicesProps } from './components/ColorChoices';
 
-class ColorSelector extends Component<ColorSelectorProps> {
-  render(): JSX.Element {
-    return (
-      <Grid item xs={12} container>
-        <Grid item xs={12} container alignItems={'flex-end'}>
-          <Grid item>
-            <Typography variant={'h6'}>
-              {`Subject Color: ${this.props.colorName}`}
-            </Typography>
-          </Grid>
-        </Grid>
-
-        <Grid container alignItems={'center'} spacing={2}>
-          <ColorChoices />
-
-          <PreviewCard
-            color={this.props.color}
-            secondaryColor={this.props.secondaryColor}
-            selectedIcon={this.props.selectedIcon}
-            subjectName={this.props.subjectName}
-          />
+const ColorSelector = (props: ColorSelectorProps) => {
+  return (
+    <Grid item xs={12} container>
+      <Grid item xs={12} container alignItems={'flex-end'}>
+        <Grid item>
+          <Typography variant={'h6'}>
+            {`Subject Color: ${props.selectedColorName}`}
+          </Typography>
         </Grid>
       </Grid>
-    );
-  }
+
+      <Grid container alignItems={'center'} spacing={2}>
+        <ColorChoices />
+
+        <PreviewCard />
+      </Grid>
+    </Grid>
+  );
+};
+
+export interface ColorSelectorProps {
+  selectedColorName: string;
 }
 
-export interface ColorSelectorProps extends WithStyles<typeof styles> {
-  color: string;
-  colorName: string;
-  subjectName: string;
-  secondaryColor: string;
-  selectedIcon: OverridableComponent<SvgIconTypeMap>;
-}
+const mapStateToProps = (state: State): ColorSelectorProps => {
+  return ({
+    selectedColorName: state.subjectListState.selectedColor.name,
+  } as unknown) as ColorChoicesProps;
+};
 
-export default withStyles(styles, { withTheme: true })(ColorSelector);
+export default connect(mapStateToProps)(ColorSelector);
