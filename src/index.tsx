29072cbca02858d6a0381aeb00f2039aec0c1aa3
jyarbrough <jcyarbro88@gmail.com>
2021-01-12 19:27:38 -0600

    moving firebase init

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
 import { Route } from 'react-router';
 import { Provider } from 'react-redux';
 import { initApp } from './creators/init';
@@ -14,6 +15,20 @@ import WeeklyPlanner from './components/widgets/weekly-planner/WeeklyPlannerConn
 
 const history = createHashHistory(),
   store = createStore(history);
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyB5ipECed_z0KkTtOcgISndCMxyy7mLaG4',
+  authDomain: 'teacher-app-41ea7.firebaseapp.com',
+  databaseURL: 'https://teacher-app-41ea7-default-rtdb.firebaseio.com',
+  projectId: 'teacher-app-41ea7',
+  storageBucket: 'teacher-app-41ea7.appspot.com',
+  messagingSenderId: '315614411494',
+  appId: '1:315614411494:web:8853e55e11489ead821927',
+  measurementId: 'G-KGKB1RCQDK',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 store.dispatch(initApp());
 
