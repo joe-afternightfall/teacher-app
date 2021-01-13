ae6c2ea23df58d6bf28017c9d04838bc629f20e6
jyarbrough <jcyarbro88@gmail.com>
2021-01-12 23:02:21 -0600

    init cleanup

--- src/creators/init.ts
+++ src/creators/init.ts
@@ -1,18 +1,15 @@
 import actions from './actions';
-import { subjectList, weeklyPlanners } from '../configs/dummy-data';
+import { weeklyPlanners } from '../configs/dummy-data';
 import { Planner } from '../configs/types/WeeklyPlanner';
-import { Subject } from '../configs/types/Subject';
 
 export const initApp = (): InitAction => {
   return {
     type: actions.INITIALIZE,
     weeklyPlanners: weeklyPlanners,
-    subjectList: subjectList,
   };
 };
 
 export interface InitAction {
   type: string;
   weeklyPlanners: Planner[];
-  subjectList: Subject[];
 }
