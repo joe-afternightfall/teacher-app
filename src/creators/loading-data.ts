6e12df245da580ebdb4de60f25d417bd822855ef
jyarbrough <jcyarbro88@gmail.com>
2021-01-12 19:30:06 -0600

    using new save method

---
+++ src/creators/loading-data.ts
@@ -0,0 +1,19 @@
+import actions from './actions';
+
+export const updatingSubjectInfo = () => {
+  return {
+    type: actions.UPDATING_SUBJECT_INFO,
+  };
+};
+
+export const subjectSaveComplete = () => {
+  return {
+    type: actions.SUBJECT_SAVE_COMPLETE,
+  };
+};
+
+export const subjectSaveFailed = () => {
+  return {
+    type: actions.SUBJECT_SAVE_FAILED,
+  };
+};
--- src/creators/subject-list.ts
+++ src/creators/subject-list.ts
@@ -1,7 +1,15 @@
+import {
+  updatingSubjectInfo,
+  subjectSaveFailed,
+  subjectSaveComplete,
+} from './loading-data';
 import actions from './actions';
-import { ColorChoice } from '../configs/theme/subject-color-choices';
+import firebase from 'firebase';
+import { ThunkAction } from 'redux-thunk';
+import { AnyAction, Dispatch } from 'redux';
+import { State } from '../configs/redux/store';
 import { Subject } from '../configs/types/Subject';
-import { saveSubject } from '../services/subject-list';
+import { ColorChoice } from '../configs/theme/subject-color-choices';
 
 export const selectColor = (color: ColorChoice): SelectColorAction => {
   return {
@@ -41,19 +49,42 @@ export const closeSubjectInfoDialog = () => {
   };
 };
 
-export const saveSubjectInfo = (subject: Subject) => {
-  console.log('subject: ' + JSON.stringify(subject));
+export const saveSubjectInfo = (
+  subject: Subject
+): ThunkAction<void, State, void, AnyAction> => async (
+  dispatch: Dispatch,
+  getState: () => State
+): Promise<void> => {
+  const subjectListRef = firebase.database().ref('/subjects');
+  const newSubjectRef = subjectListRef.push();
 
-  saveSubject(subject)
-    .then((response) => {
-      console.log('response: ' + JSON.stringify(response));
-      // (dispatch as ThunkDispatch<State, void, AnyAction>)(login(username));
-    })
-    .catch((error: Error) => {
-      console.log(`failed auth with error: ${error}`);
+  dispatch(updatingSubjectInfo());
+
+  return await newSubjectRef.set(
+    {
+      id: subject.id,
+      subjectName: subject.subjectName,
+      primaryColorId: subject.primaryColorId,
+      primaryColor: subject.primaryColor,
+      secondaryColor: subject.secondaryColor,
+      iconId: subject.iconId,
+    },
+    (error) => {
+      if (error) {
+        dispatch(subjectSaveFailed());
+      } else {
+        dispatch(subjectSaveComplete());
+      }
+    }
+  );
+};
+
+export const getSubjects = async () => {
+  return await firebase
+    .database()
+    .ref('/subjects')
+    .once('value')
+    .then((snapshot) => {
+      return snapshot.val();
     });
-
-  return {
-    type: actions.SAVE_SUBJECT_INFO,
-  };
 };
