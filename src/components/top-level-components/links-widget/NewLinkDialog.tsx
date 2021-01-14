b603e3de42e816cd9c10d301d682bcbaf7df0b15
jyarbrough <jcyarbro88@gmail.com>
2021-01-14 16:10:20 -0600

    setting up new link dialog

--- src/components/top-level-components/links-widget/NewLinkDialog.tsx
+++ src/components/top-level-components/links-widget/NewLinkDialog.tsx
@@ -9,10 +9,15 @@ import {
   DialogActions,
   DialogContent,
 } from '@material-ui/core';
-import { Dispatch } from 'redux';
+import { v4 as uuidv4 } from 'uuid';
 import { connect } from 'react-redux';
+import { AnyAction, Dispatch } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
 import CloseIcon from '@material-ui/icons/Close';
 import { State } from '../../../configs/redux/store';
+import LinkForm from '../../widgets/link-form/LinkForm';
+import { Subject } from '../../../configs/types/Subject';
+import { saveLinkInfo } from '../../../services/link-service';
 import { closeLinkDialog } from '../../../creators/link-dialog';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
@@ -27,8 +32,37 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+export interface NewLinkForm {
+  id: string;
+  linkUrl: string;
+  linkTitle: string;
+  subjectId: string;
+}
+
 const NewLinkDialog = (props: NewLinkDialogProps): JSX.Element => {
   const classes = useStyles();
+
+  const [values, setValues] = React.useState<NewLinkForm>({
+    id: uuidv4(),
+    linkUrl: '',
+    linkTitle: '',
+    subjectId: '',
+  });
+
+  const dropdownChangeHandler = (
+    e: React.ChangeEvent<{ name?: string; value: unknown }>
+  ) => {
+    setValues((oldValues: NewLinkForm) => ({
+      ...oldValues,
+      [e.target.name as string]: e.target.value,
+    }));
+  };
+
+  const textfieldChangeHandler = (name: keyof NewLinkForm) => (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setValues({ ...values, [name]: e.target.value });
+  };
 
   return (
     <Dialog
@@ -48,26 +82,30 @@ const NewLinkDialog = (props: NewLinkDialogProps): JSX.Element => {
         </IconButton>
       </DialogTitle>
 
-      <DialogContent>
-        <Grid
-          container
-          spacing={2}
-          justify={'center'}
-          direction={'column'}
-          style={{
-            minHeight: '50vh',
-          }}
-          alignItems={'center'}
-        >
+      <DialogContent style={{ margin: '24px 0' }}>
+        <Grid container justify={'center'} alignItems={'center'}>
           <Grid item>
-            <Typography variant={'h6'}>{`Add New Link Dialog`}</Typography>
+            <Typography variant={'h6'}>{'Link Information'}</Typography>
           </Grid>
+
+          <LinkForm
+            dropdownChangeHandler={dropdownChangeHandler}
+            textfieldChangeHandler={textfieldChangeHandler}
+            linkValues={values}
+            subjectList={props.subjectList}
+          />
         </Grid>
       </DialogContent>
 
       <DialogActions>
         <Button onClick={props.closeDialogHandler}>{'Cancel'}</Button>
-        <Button>{'Save'}</Button>
+        <Button
+          onClick={() => {
+            props.saveClickHandler(values);
+          }}
+        >
+          {'Save'}
+        </Button>
       </DialogActions>
     </Dialog>
   );
@@ -77,11 +115,14 @@ export interface NewLinkDialogProps {
   displayName: string;
   displayLinkDialog: boolean;
   closeDialogHandler: () => void;
+  subjectList: Subject[];
+  saveClickHandler: (link: NewLinkForm) => void;
 }
 
 const mapStateToProps = (state: State): NewLinkDialogProps => {
   return ({
     displayLinkDialog: state.applicationState.displayLinkDialog,
+    subjectList: state.subjectListState.subjectList,
   } as unknown) as NewLinkDialogProps;
 };
 
@@ -89,6 +130,9 @@ const mapDispatchToProps = (dispatch: Dispatch): NewLinkDialogProps =>
   (({
     closeDialogHandler: () => {
       dispatch(closeLinkDialog());
+    },
+    saveClickHandler: (link: NewLinkForm) => {
+      (dispatch as ThunkDispatch<State, void, AnyAction>)(saveLinkInfo(link));
     },
   } as unknown) as NewLinkDialogProps);
 
