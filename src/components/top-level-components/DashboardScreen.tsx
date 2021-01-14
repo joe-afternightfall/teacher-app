c169c8a7bd4a70312799202486acc4e97afe5358
jyarbrough <jcyarbro88@gmail.com>
2021-01-13 19:17:07 -0600

    adding data table for links

--- src/components/top-level-components/DashboardScreen.tsx
+++ src/components/top-level-components/DashboardScreen.tsx
@@ -5,22 +5,19 @@ import {
   StyledComponentProps,
 } from '@material-ui/core/styles';
 import React, { Component } from 'react';
+import { Grid } from '@material-ui/core';
 import { Styles } from '@material-ui/styles';
+import LinksWidget from './links-widget/LinksWidget';
 import PlannerWidget from './planner-widget/PlannerWidget';
-import { Card, CardHeader, Grid } from '@material-ui/core';
 
 const styles: Styles<Theme, StyledComponentProps> = () => ({});
 
 class DashboardScreen extends Component<DashboardScreenProps> {
   render(): JSX.Element {
-    const { classes } = this.props;
-
     return (
       <Grid container spacing={2}>
         <Grid item xs={6}>
-          <Card>
-            <CardHeader title={'Empty Card'} />
-          </Card>
+          <LinksWidget />
         </Grid>
 
         <Grid item xs={6}>
