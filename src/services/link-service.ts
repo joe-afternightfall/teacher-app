e15e7ad76252f249e6b8cdbfcefab46231276bd1
jyarbrough <jcyarbro88@gmail.com>
2021-01-15 08:26:28 -0600

    adding delete method

--- src/services/link-service.ts
+++ src/services/link-service.ts
@@ -3,9 +3,12 @@ import { v4 as uuidv4 } from 'uuid';
 import { ThunkAction } from 'redux-thunk';
 import { AnyAction, Dispatch } from 'redux';
 import { State } from '../configs/redux/store';
-import { closeLinkDialog } from '../creators/link-dialog';
+import {
+  closeDeleteLinkDialog,
+  closeLinkDialog,
+} from '../creators/topic-links-dialog';
 import { displayAppSnackbar } from '../creators/app-snackbar';
-import { NewLinkForm } from '../components/top-level-components/links-widget/NewLinkDialog';
+import { NewLinkForm } from '../components/widgets/topic-links/components/NewLinkDialog';
 
 export const saveLinkInfo = (
   link: NewLinkForm
@@ -34,6 +37,29 @@ export const saveLinkInfo = (
       }
     }
   );
+};
+
+export const deleteLink = (): ThunkAction<
+  void,
+  State,
+  void,
+  AnyAction
+> => async (dispatch: Dispatch, getState: () => State): Promise<void> => {
+  const id = getState().applicationState.deleteLinkId;
+  return await firebase
+    .database()
+    .ref('/links')
+    .child(id)
+    .remove((error) => {
+      if (error) {
+        // todo: dispatch error snackbar
+      } else {
+        dispatch(displayAppSnackbar('Deleted Link'));
+        setTimeout(() => {
+          dispatch(closeDeleteLinkDialog());
+        }, 1000);
+      }
+    });
 };
 
 export const getLinksList = async () => {
