d96bca20a38878573408dad65744752b29924e58
jyarbrough <jcyarbro88@gmail.com>
2021-01-15 21:19:47 -0600

    adding test

---
+++ src/utils/validate-name.test.ts
@@ -0,0 +1,52 @@
+import { checkForDuplicates } from "./validate-name";
+import { v4 as uuidv4 } from "uuid";
+
+describe('validate name util', () => {
+  const firstId = uuidv4();
+  const secondId = uuidv4();
+
+  const firstSubjectName = uuidv4();
+  const secondSubjectName = uuidv4();
+
+  const subjects = [
+    {
+      firebaseId: '',
+      id: firstId,
+      subjectName: firstSubjectName,
+      primaryColorId: uuidv4(),
+      primaryColor: uuidv4(),
+      secondaryColor: uuidv4(),
+      iconId: uuidv4(),
+    },
+    {
+      firebaseId: uuidv4(),
+      id: secondId,
+      subjectName: secondSubjectName,
+      primaryColorId: uuidv4(),
+      primaryColor: uuidv4(),
+      secondaryColor: uuidv4(),
+      iconId: uuidv4(),
+    },
+    {
+      firebaseId: uuidv4(),
+      id: uuidv4(),
+      subjectName: 'subject-name',
+      primaryColorId: uuidv4(),
+      primaryColor: uuidv4(),
+      secondaryColor: uuidv4(),
+      iconId: uuidv4(),
+    },
+  ];
+
+  it('should return true when finding duplicate', () => {
+    const result = checkForDuplicates(subjects, 'subject-name');
+
+    expect(result).toEqual(true);
+  });
+
+  it('should return false when no duplicates', () => {
+    const result = checkForDuplicates(subjects, 'subjects');
+
+    expect(result).toEqual(false);
+  });
+});
