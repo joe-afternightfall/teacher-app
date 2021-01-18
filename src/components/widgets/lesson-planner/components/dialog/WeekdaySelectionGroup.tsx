2cb2b742e3464d6492be5bc0b9b53dd05959e24c
jyarbrough <jcyarbro88@gmail.com>
2021-01-18 11:01:01 -0600

    making selection group fcc

--- src/components/widgets/lesson-planner/components/dialog/WeekdaySelectionGroup.tsx
+++ src/components/widgets/lesson-planner/components/dialog/WeekdaySelectionGroup.tsx
@@ -1,35 +1,57 @@
-import {
-  Theme,
-  WithStyles,
-  withStyles,
-  StyledComponentProps,
-} from '@material-ui/core/styles';
-import React, { Component } from 'react';
-import { Styles } from '@material-ui/styles';
+import React from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { State } from '../../../../../configs/redux/store';
+import { updateSelectedDays } from '../../../../../creators/lesson-planner';
 
-const styles: Styles<Theme, StyledComponentProps> = () => ({});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {},
+  })
+);
 
-class WeekdaySelectionGroup extends Component<WeekdaySelectionGroupProps> {
-  render(): JSX.Element {
-    const { selectedDays, changeHandler } = this.props;
+const WeekdaySelectionGroup = (
+  props: WeekdaySelectionGroupProps
+): JSX.Element => {
+  const classes = useStyles();
 
-    return (
-      <ToggleButtonGroup value={selectedDays} onChange={changeHandler}>
-        <ToggleButton value={'monday'}>{'Mon'}</ToggleButton>
-        <ToggleButton value={'tuesday'}>{'Tue'}</ToggleButton>
-        <ToggleButton value={'wednesday'}>{'Wed'}</ToggleButton>
-        <ToggleButton value={'thursday'}>{'Thu'}</ToggleButton>
-        <ToggleButton value={'friday'}>{'Fri'}</ToggleButton>
-      </ToggleButtonGroup>
-    );
-  }
-}
+  return (
+    <ToggleButtonGroup
+      value={props.selectedDays}
+      onChange={props.changeHandler}
+    >
+      <ToggleButton value={'monday'}>{'Mon'}</ToggleButton>
+      <ToggleButton value={'tuesday'}>{'Tue'}</ToggleButton>
+      <ToggleButton value={'wednesday'}>{'Wed'}</ToggleButton>
+      <ToggleButton value={'thursday'}>{'Thu'}</ToggleButton>
+      <ToggleButton value={'friday'}>{'Fri'}</ToggleButton>
+    </ToggleButtonGroup>
+  );
+};
 
-export interface WeekdaySelectionGroupProps extends WithStyles<typeof styles> {
+export interface WeekdaySelectionGroupProps {
   selectedDays: string[];
   changeHandler: (event: React.MouseEvent<HTMLElement>) => void;
 }
 
-export default withStyles(styles, { withTheme: true })(WeekdaySelectionGroup);
+const mapStateToProps = (state: State): WeekdaySelectionGroupProps => {
+  return ({
+    selectedDays: state.lessonPlannerState.selectedDays,
+  } as unknown) as WeekdaySelectionGroupProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): WeekdaySelectionGroupProps =>
+  (({
+    changeHandler: (e: React.MouseEvent<HTMLElement>) => {
+      const element = e.currentTarget as HTMLInputElement;
+      dispatch(updateSelectedDays(element.value));
+    },
+  } as unknown) as WeekdaySelectionGroupProps);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(WeekdaySelectionGroup);
