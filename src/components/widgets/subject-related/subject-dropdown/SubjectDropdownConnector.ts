5472cfafc2762645a8d2b72d9081104d55344183
jyarbrough <jcyarbro88@gmail.com>
2021-01-19 21:45:33 -0600

    adding connector props

--- src/components/widgets/subject-related/subject-dropdown/SubjectDropdownConnector.ts
+++ src/components/widgets/subject-related/subject-dropdown/SubjectDropdownConnector.ts
@@ -1,16 +1,19 @@
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
-import SubjectDropdown from './SubjectDropdown';
+import SubjectDropdown, { SubjectDropdownProps } from './SubjectDropdown';
 import { State } from '../../../../configs/redux/store';
 
-const mapStateToProps = (state: State) => {
-  return {
+const mapStateToProps = (state: State): SubjectDropdownProps => {
+  return ({
     subjectList: state.subjectListState.subjectList
       ? state.subjectListState.subjectList
       : [],
-  };
+  } as unknown) as SubjectDropdownProps;
 };
 
-const mapDispatchToProps = (dispatch: Dispatch, ownProps: any) => ({});
+const mapDispatchToProps = (
+  dispatch: Dispatch,
+  ownProps: any
+): SubjectDropdownProps => (({} as unknown) as SubjectDropdownProps);
 
 export default connect(mapStateToProps, mapDispatchToProps)(SubjectDropdown);
