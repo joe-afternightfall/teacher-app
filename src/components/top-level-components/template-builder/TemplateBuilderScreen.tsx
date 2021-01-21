7ed9cded0e0ce1b689a8c9a477f51e83eb5a6333
jyarbrough <jcyarbro88@gmail.com>
2021-01-20 22:12:04 -0600

    got working

--- src/components/top-level-components/template-builder/TemplateBuilderScreen.tsx
+++ src/components/top-level-components/template-builder/TemplateBuilderScreen.tsx
@@ -6,39 +6,28 @@ import {
 } from '@material-ui/core/styles';
 import React, { Component } from 'react';
 import { Styles } from '@material-ui/styles';
-import { Grid } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 import LessonPlannerComp from '../../widgets/lesson-planner/LessonPlannerConnector';
-import {
-  buildAndSaveDefaultTemplate,
-  getTemplateBuilder,
-} from '../../../services/template-builder-service';
 import { LessonPlanner } from '../../../configs/types/LessonPlanner';
 import TemplateBuilderControls from '../../widgets/lesson-planner/components/planner-controls/TemplateBuilderControls';
+import { buildAndSaveDefaultTemplate } from '../../../services/template-builder-service';
 
 const styles: Styles<Theme, StyledComponentProps> = () => ({});
 
 class TemplateBuilderScreen extends Component<TemplateBuilderScreenProps> {
-  async componentDidMount() {
-    const templateBuilder: any = await getTemplateBuilder();
-
-    if (templateBuilder === null) {
-      console.log('generating new template');
-      await buildAndSaveDefaultTemplate();
-      const dataSnapshot: any = await getTemplateBuilder();
-      console.log('dataSnapshot: ' + JSON.stringify(dataSnapshot));
-      //todo: remove this call when setup index listeners
-      this.props.loadTemplateBuilderHandler(dataSnapshot);
-    } else {
-      this.props.loadTemplateBuilderHandler(templateBuilder);
-    }
-  }
-
   render(): JSX.Element {
     const { classes } = this.props;
+
+    const handleClick = async () => {
+      const newVar = await buildAndSaveDefaultTemplate();
+      this.props.loadTemplateBuilderHandler(newVar);
+    };
 
     return (
       <Grid>
         <TemplateBuilderControls />
+
+        <Button>{'Generate Template'}</Button>
 
         <LessonPlannerComp />
       </Grid>
