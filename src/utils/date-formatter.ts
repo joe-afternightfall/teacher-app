fc560ae9ebd393c5ff95dfc15313582cde204f00
jyarbrough <jcyarbro88@gmail.com>
2021-01-22 13:50:01 -0600

    adding to util

--- src/utils/date-formatter.ts
+++ src/utils/date-formatter.ts
@@ -3,3 +3,7 @@ import { format } from 'date-fns';
 export const formatDate = (date: Date) => {
   return format(new Date(date), 'h:mm a');
 };
+
+export const formatDateWithout = (date: Date) => {
+  return format(new Date(date), 'h:mm');
+};
