4d6c96431765da6a52f03bcc6cc2d3ab72582c53
jyarbrough <jcyarbro88@gmail.com>
2021-01-24 16:01:29 -0600

    done

---
+++ src/reducers/bookmarks.ts
@@ -0,0 +1,33 @@
+import { AnyAction } from 'redux';
+import actions from '../creators/actions';
+import { Bookmark } from '../configs/types/Bookmark';
+
+export default {
+  reducer(
+    state: BookmarksState = ({} as unknown) as BookmarksState,
+    action: AnyAction
+  ): BookmarksState {
+    let newState = Object.assign({}, state);
+
+    switch (action.type) {
+      case actions.LOAD_BOOKMARKS_LIST:
+        newState.bookmarks = action.bookmarks;
+        break;
+      case actions.OPEN_NEW_BOOKMARK_DIALOG:
+        newState.displayNewBookmarkDialog = true;
+        break;
+      case actions.CLOSE_NEW_BOOKMARK_DIALOG:
+        newState.displayNewBookmarkDialog = false;
+        break;
+      default:
+        newState = state;
+    }
+
+    return newState;
+  },
+};
+
+export interface BookmarksState {
+  bookmarks: Bookmark[];
+  displayNewBookmarkDialog: boolean;
+}
