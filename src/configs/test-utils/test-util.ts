bea65248e387f3e349e77d228c929c55735fdb4b
jyarbrough <jcyarbro88@gmail.com>
2021-01-24 09:37:09 -0600

    handling undefined

--- src/configs/test-utils/test-util.ts
+++ src/configs/test-utils/test-util.ts
@@ -100,4 +100,4 @@ export const buildColor = (): ColorChoice => {
     primaryColor: 'primary',
     secondaryColor: 'secondary',
   };
-}
+};
--- src/reducers/subject-list.test.ts
+++ src/reducers/subject-list.test.ts
@@ -1,14 +1,10 @@
-import lessonPlanner from './lesson-planner';
 import actions from '../creators/actions';
-import { lessonPlanners } from '../configs/dummy-data';
 import subjectList from '../reducers/subject-list';
-import { buildColor, buildLessonPlanner, buildSubjectList } from '../configs/test-utils/test-util';
-import { State } from '../configs/redux/store';
-
+import { buildColor, buildSubjectList } from '../configs/test-utils/test-util';
 
 describe('subject list reducer', () => {
   it('should return action', () => {
-    let subjects = buildSubjectList();
+    const subjects = buildSubjectList();
     const state = subjectList.reducer(undefined, {
       type: actions.INITIALIZE,
       subjectList: subjects,
@@ -22,7 +18,7 @@ describe('subject list reducer', () => {
 
     const state = subjectList.reducer(undefined, {
       type: actions.SELECT_COLOR,
-      selectedColor: color
+      selectedColor: color,
     });
 
     expect(state.selectedColor).toEqual(color);
@@ -30,7 +26,7 @@ describe('subject list reducer', () => {
 
   it('should return true for display subject info', () => {
     const state = subjectList.reducer(undefined, {
-      type: actions.OPEN_SUBJECT_INFO_DIALOG
+      type: actions.OPEN_SUBJECT_INFO_DIALOG,
     });
 
     expect(state.displaySubjectInfo).toEqual(true);
@@ -38,7 +34,7 @@ describe('subject list reducer', () => {
 
   it('should return false for display subject info', () => {
     const state = subjectList.reducer(undefined, {
-      type: actions.CLOSE_SUBJECT_INFO_DIALOG
+      type: actions.CLOSE_SUBJECT_INFO_DIALOG,
     });
 
     expect(state.displaySubjectInfo).toEqual(false);
@@ -46,7 +42,7 @@ describe('subject list reducer', () => {
 
   it('should return true for display loader', () => {
     const state = subjectList.reducer(undefined, {
-      type: actions.UPDATING_SUBJECT_INFO
+      type: actions.UPDATING_SUBJECT_INFO,
     });
 
     expect(state.displayLoader).toEqual(true);
@@ -54,7 +50,7 @@ describe('subject list reducer', () => {
 
   it('should return false for display loader', () => {
     const state = subjectList.reducer(undefined, {
-      type: actions.SUBJECT_SAVE_COMPLETE
+      type: actions.SUBJECT_SAVE_COMPLETE,
     });
 
     expect(state.displayLoader).toEqual(false);
@@ -62,7 +58,7 @@ describe('subject list reducer', () => {
 
   it('should return false for editing form', () => {
     const state = subjectList.reducer(undefined, {
-      type: actions.CLEAR_EDITING
+      type: actions.CLEAR_EDITING,
     });
 
     expect(state.editingForm).toEqual(false);
