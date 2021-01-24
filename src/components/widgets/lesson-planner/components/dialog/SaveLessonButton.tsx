f47d2e8c2c6727c5939021a3e86b6a79e92a60f1
jyarbrough <jcyarbro88@gmail.com>
2021-01-24 12:48:18 -0600

    creating button

---
+++ src/components/widgets/lesson-planner/components/dialog/SaveLessonButton.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { AnyAction, Dispatch } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+import SaveIcon from '@material-ui/icons/Save';
+import { Button, Fab, IconButton, Tooltip } from '@material-ui/core';
+import { State } from '../../../../../configs/redux/store';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { updateLessonBoardOrder } from '../../../../../services/template-builder-service';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {},
+  })
+);
+
+const SaveLessonButton = (props: SaveLessonButtonProps): JSX.Element => {
+  const classes = useStyles();
+
+  return (
+    <Tooltip title={'Save Lesson'}>
+      <Button
+        color={'secondary'}
+        variant={'contained'}
+        onClick={() => {
+          props.saveHandler();
+        }}
+        startIcon={<SaveIcon />}
+        disabled={props.isDisabled}
+      >
+        {'Save'}
+      </Button>
+    </Tooltip>
+  );
+};
+
+export interface SaveLessonButtonProps {
+  isDisabled: boolean;
+  saveHandler: () => void;
+}
+
+const mapStateToProps = (state: State): SaveLessonButtonProps => {
+  return ({
+    isDisabled: !state.lessonPlannerState.lessonBoardChanged,
+  } as unknown) as SaveLessonButtonProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): SaveLessonButtonProps =>
+  (({
+    saveHandler: () => {
+      (dispatch as ThunkDispatch<State, void, AnyAction>)(
+        updateLessonBoardOrder()
+      );
+    },
+  } as unknown) as SaveLessonButtonProps);
+
+export default connect(mapStateToProps, mapDispatchToProps)(SaveLessonButton);
