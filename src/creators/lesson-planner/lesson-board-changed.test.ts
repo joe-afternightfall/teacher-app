616f7c367ede161f990f0fd14275a906417c1c71
jyarbrough <jcyarbro88@gmail.com>
2021-01-25 09:32:47 -0600

    adding coverage

---
+++ src/creators/lesson-planner/lesson-board-changed.test.ts
@@ -0,0 +1,12 @@
+import actions from '../actions';
+import { lessonBoardChanged } from './lesson-board-changed';
+
+describe('lesson board changed creator', () => {
+  it('should return LESSON_BOARD_CHANGE action', () => {
+    const action = lessonBoardChanged();
+
+    expect(action).toEqual({
+      type: actions.LESSON_BOARD_CHANGE,
+    })
+  });
+});
