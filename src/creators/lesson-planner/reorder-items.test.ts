91a57af85584c09b6b764a7a90806b0773b90b8a
jyarbrough <jcyarbro88@gmail.com>
2021-01-25 10:22:09 -0600

    adding coverage

---
+++ src/creators/lesson-planner/reorder-items.test.ts
@@ -0,0 +1,17 @@
+import actions from '../actions';
+import { reorderPlannerItems } from './reorder-items';
+import { buildLessonItems } from '../../configs/test-utils/test-util';
+
+describe('reorder items creator', () => {
+  it('should return REORDER_LESSON_PLANNER action', () => {
+    const items = buildLessonItems(3);
+    const action = reorderPlannerItems(items, 'wednesday', true);
+
+    expect(action).toEqual({
+      type: actions.REORDER_LESSON_PLANNER,
+      items: items,
+      dayOfWeek: 'wednesday',
+      isTemplate: true,
+    })
+  });
+});
