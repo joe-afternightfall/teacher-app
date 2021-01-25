1d7b24bb7bd17f62250a15f6424427b36bae5c4f
jyarbrough <jcyarbro88@gmail.com>
2021-01-25 10:28:29 -0600

    adding test

---
+++ src/creators/lesson-planner/move-items.test.ts
@@ -0,0 +1,24 @@
+import actions from '../actions';
+import { movePlannerItems } from './move-items';
+import { buildLessonItems } from '../../configs/test-utils/test-util';
+
+describe('move items creator', () => {
+  it('should return action', () => {
+    const items = buildLessonItems(3);
+    const planner = {
+      monday: items,
+      tuesday: items,
+      wednesday: items,
+      thursday: items,
+      friday: items
+    }
+
+    const action = movePlannerItems(planner, true);
+
+    expect(action).toEqual({
+      type: actions.MOVE_PLANNER_ITEMS,
+      days: planner,
+      isTemplate: true,
+    });
+  });
+});
