1e0982ffbb26c9c6a4ca92f3ec8e9b32cef35efd
jyarbrough <jcyarbro88@gmail.com>
2021-01-27 12:33:23 -0600

    updating model objects

--- src/configs/models/LessonPlanner.ts
+++ src/configs/models/LessonPlanner.ts
@@ -1,30 +1,23 @@
+import { LessonPlannerDAO } from './LessonPlannerDAO';
 import { LessonNote, Weekday } from '../types/LessonPlanner';
 
-export class LessonPlanner {
+export class LessonPlanner extends LessonPlannerDAO {
   firebaseId: string;
-  updatedAt: Date;
-  id: string;
-  title: string;
-  weekdays: {
-    [key: string]: Weekday;
-  };
-  notes: LessonNote[];
 
   constructor(
     firebaseId: string,
     updatedAt: Date,
     id: string,
     title: string,
+    startDate: Date,
+    endDate: Date,
     weekdays: {
       [key: string]: Weekday;
     },
     notes: LessonNote[]
   ) {
+    super(updatedAt, id, title, startDate, endDate, weekdays, notes);
+
     this.firebaseId = firebaseId;
-    this.updatedAt = updatedAt;
-    this.id = id;
-    this.title = title;
-    this.weekdays = weekdays;
-    this.notes = notes;
   }
 }
