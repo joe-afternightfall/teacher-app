b3845bd2b07f90d8cdf3298dd96da4192c0d0c64
joe-afternightfall <joe@afternightfall.com>
2021-01-29 14:27:58 -0600

    app bar setup

--- src/components/app-shell/AppBar.tsx
+++ src/components/app-shell/AppBar.tsx
@@ -1,38 +1,96 @@
+import clsx from 'clsx';
 import React from 'react';
-import SideDrawer from './SideDrawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { State } from '../../configs/redux/store';
+import { toggleSideDrawer } from '../../creators/application/side-drawer';
+
+export const DRAWER_SIZE = '45vw';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      marginBottom: theme.spacing(3),
+    appBar: {
+      color: '#fff',
+      zIndex: theme.zIndex.drawer + 1,
+      // color: theme.palette.text.primary,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
     },
-    title: {
+    appBarShift: {
+      marginLeft: DRAWER_SIZE,
+      width: `calc(100% - ${DRAWER_SIZE})`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    hide: {
+      display: 'none',
+    },
+    logoWrapper: {
       flexGrow: 1,
     },
   })
 );
 
-export default function TopAppBar(): JSX.Element {
+function TopAppBar(props: AppBarProps): JSX.Element {
   const classes = useStyles();
 
+  const open = props.open;
+
   return (
-    <AppBar position={'static'} className={classes.root}>
+    <AppBar
+      position={'fixed'}
+      className={clsx(classes.appBar, {
+        [classes.appBarShift]: open,
+      })}
+    >
       <Toolbar>
-        <SideDrawer />
-        <Typography
-          variant={'h6'}
-          className={classes.title}
-          data-testid={'app-bar-title'}
+        <IconButton
+          edge={'start'}
+          color={'inherit'}
+          onClick={props.toggleSideDrawerHandler}
+          data-testid={'toggle-app-drawer-button'}
+          aria-label={'open drawer'}
+          className={clsx({
+            [classes.hide]: open,
+          })}
         >
-          {"Jamie's Teacher App"}
-        </Typography>
-        <AccountCircle />
+          <MenuIcon />
+        </IconButton>
+        {/*<div className={classes.logoWrapper}>*/}
+        {/*  */}
+        {/*</div>*/}
       </Toolbar>
     </AppBar>
   );
 }
+
+export interface AppBarProps {
+  open: boolean;
+  toggleSideDrawerHandler: () => void;
+}
+
+const mapStateToProps = (state: State): AppBarProps => {
+  return ({
+    open: state.applicationState.openSideDrawer,
+  } as unknown) as AppBarProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): AppBarProps =>
+  (({
+    toggleSideDrawerHandler: (): void => {
+      dispatch(toggleSideDrawer());
+    },
+  } as unknown) as AppBarProps);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopAppBar);
