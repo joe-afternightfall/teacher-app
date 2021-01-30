49c8119882719e75bba74ff1d2c9a713178daffd
joe-afternightfall <joe@afternightfall.com>
2021-01-29 19:46:12 -0600

    ripped out to comp

--- src/TestDrawer.tsx
+++ src/TestDrawer.tsx
@@ -14,6 +14,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import ResponsiveSideDrawer from './components/app-shell/ResponsiveSideDrawer';
 
 const drawerWidth = 240;
 
@@ -22,29 +23,29 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       display: 'flex',
     },
-    drawer: {
-      [theme.breakpoints.up('sm')]: {
-        width: drawerWidth,
-        flexShrink: 0,
-      },
-    },
+    // drawer: {
+    //   [theme.breakpoints.up('sm')]: {
+    //     width: drawerWidth,
+    //     flexShrink: 0,
+    //   },
+    // },
     appBar: {
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up('md')]: {
         width: `calc(100% - ${drawerWidth}px)`,
         marginLeft: drawerWidth,
       },
     },
     menuButton: {
       marginRight: theme.spacing(2),
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up('md')]: {
         display: 'none',
       },
     },
     // necessary for content to be below app bar
     toolbar: theme.mixins.toolbar,
-    drawerPaper: {
-      width: drawerWidth,
-    },
+    // drawerPaper: {
+    //   width: drawerWidth,
+    // },
     content: {
       flexGrow: 1,
       padding: theme.spacing(3),
@@ -52,26 +53,26 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window;
-}
+// interface Props {
+//   /**
+//    * Injected by the documentation to work in an iframe.
+//    * You won't need it on your project.
+//    */
+//   window?: () => Window;
+// }
 
-export default function ResponsiveDrawer(props: Props) {
-  const { window } = props;
+export default function ResponsiveDrawer() {
+  // const { window } = props;
   const classes = useStyles();
-  const theme = useTheme();
+  // const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
+  // const container =
+  //   window !== undefined ? () => window().document.body : undefined;
 
   return (
     <div className={classes.root}>
@@ -92,40 +93,43 @@ export default function ResponsiveDrawer(props: Props) {
           </Typography>
         </Toolbar>
       </AppBar>
-      <nav className={classes.drawer} aria-label="mailbox folders">
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Hidden smUp implementation="css">
-          <Drawer
-            container={container}
-            variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-          >
-            {<NestedList />}
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation="css">
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant="permanent"
-            open
-          >
-            {<NestedList />}
-          </Drawer>
-        </Hidden>
-      </nav>
+
+      <ResponsiveSideDrawer
+        mobileOpen={mobileOpen}
+        handleDrawerToggle={handleDrawerToggle}
+      />
+      {/*<nav className={classes.drawer}>*/}
+      {/*  <Hidden smUp>*/}
+      {/*    <Drawer*/}
+      {/*      container={container}*/}
+      {/*      variant="temporary"*/}
+      {/*      anchor={theme.direction === 'rtl' ? 'right' : 'left'}*/}
+      {/*      open={mobileOpen}*/}
+      {/*      onClose={handleDrawerToggle}*/}
+      {/*      classes={{*/}
+      {/*        paper: classes.drawerPaper,*/}
+      {/*      }}*/}
+      {/*      ModalProps={{*/}
+      {/*        keepMounted: true, // Better open performance on mobile.*/}
+      {/*      }}*/}
+      {/*    >*/}
+      {/*      {<NestedList />}*/}
+      {/*    </Drawer>*/}
+      {/*  </Hidden>*/}
+      {/*  <Hidden xsDown>*/}
+      {/*    <Drawer*/}
+      {/*      classes={{*/}
+      {/*        paper: classes.drawerPaper,*/}
+      {/*      }}*/}
+      {/*      variant="permanent"*/}
+      {/*      open*/}
+      {/*    >*/}
+      {/*      {<NestedList />}*/}
+      {/*    </Drawer>*/}
+      {/*  </Hidden>*/}
+      {/*</nav>*/}
       <main className={classes.content}>
         <div className={classes.toolbar} />
-
       </main>
     </div>
   );
