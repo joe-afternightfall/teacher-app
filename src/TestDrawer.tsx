f0f5b395f306c874b2f728e2e8090e5d67d8469f
joe-afternightfall <joe@afternightfall.com>
2021-01-29 21:10:09 -0600

    ripping out

--- src/TestDrawer.tsx
+++ src/TestDrawer.tsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ResponsiveSideDrawer from './components/app-shell/ResponsiveSideDrawer';
-import { DRAWER_SIZE } from './configs/constants/drawer-size';
+import AppBar from './components/app-shell/AppBar';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: 'flex',
-    },
-    appBar: {
-      [theme.breakpoints.up('md')]: {
-        width: `calc(100% - ${DRAWER_SIZE})`,
-        marginLeft: DRAWER_SIZE,
-      },
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-      [theme.breakpoints.up('md')]: {
-        display: 'none',
-      },
     },
     // necessary for content to be below app bar
     toolbar: theme.mixins.toolbar,
@@ -37,34 +20,20 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function ResponsiveDrawer() {
   const classes = useStyles();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
+    setOpen(!open);
   };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Responsive drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
+
+      <AppBar tempToggleSideDrawerHandler={handleDrawerToggle} />
 
       <ResponsiveSideDrawer
-        mobileOpen={mobileOpen}
+        open={open}
         handleDrawerToggle={handleDrawerToggle}
       />
 
