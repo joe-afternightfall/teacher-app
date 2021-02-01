b2c519ad31fd1aa4594f762e5c9f6afcd43b7ed6
joe-afternightfall <joe@afternightfall.com>
2021-01-31 21:27:15 -0600

    setting up builder controls

--- src/components/widgets/lesson-planner/components/planner-controls/TemplateBuilderControls.tsx
+++ src/components/widgets/lesson-planner/components/planner-controls/TemplateBuilderControls.tsx
@@ -17,53 +17,23 @@ export default function TemplateBuilderControls(): JSX.Element {
   const classes = useStyles();
 
   return (
-    <Grid container className={classes.root}>
-      <Grid item xs={3} container alignItems={'center'}>
-        <Grid item>
-          <DateInput />
-        </Grid>
+    <Grid
+      container
+      spacing={2}
+      justify={'flex-end'}
+      alignItems={'center'}
+      className={classes.root}
+    >
+      <Grid item>
+        <DateInput />
       </Grid>
 
-      <Grid item xs={6} container justify={'center'}>
-        <Grid item>
-          {/*todo:  extract out purple colors to theme*/}
-          <Typography
-            style={{
-              color: '#6F55F2',
-              // color: '#887af5',
-              // color: '#674bf2',
-              // color: '#8b78f5',
-            }}
-            variant={'h6'}
-          >
-            {'Template Builder'}
-          </Typography>
-        </Grid>
-
-        <Grid item>
-          <Typography variant={'h6'}>
-            {
-              'build your class schedule to use as a template for lesson planning'
-            }
-          </Typography>
-        </Grid>
+      <Grid item>
+        <SaveLessonButton />
       </Grid>
 
-      <Grid
-        item
-        xs={3}
-        container
-        spacing={2}
-        justify={'flex-end'}
-        alignItems={'center'}
-      >
-        <Grid item>
-          <SaveLessonButton />
-        </Grid>
-
-        <Grid item>
-          <NewTemplateButton />
-        </Grid>
+      <Grid item>
+        <NewTemplateButton />
       </Grid>
     </Grid>
   );
