0f0df0c0b7736d0766cfdb4e5c7bcd915cba1ac9
joe-afternightfall <joe@afternightfall.com>
2021-01-31 18:42:02 -0600

    adding custom theme properties

--- src/configs/theme/light-theme.ts
+++ src/configs/theme/light-theme.ts
@@ -1,8 +1,38 @@
-import createMuiTheme, { Theme } from '@material-ui/core/styles/createMuiTheme';
+import createMuiTheme, {
+  Theme,
+  ThemeOptions,
+} from '@material-ui/core/styles/createMuiTheme';
+import { Palette } from '@material-ui/core/styles/createPalette';
+
+interface CustomPalette extends Palette {
+  colors: {
+    active: {
+      highlight: string;
+      hover: string;
+    };
+  };
+}
+
+export interface AppTheme extends Theme {
+  palette: CustomPalette;
+}
+
+interface AppThemeOptions extends ThemeOptions {
+  palette: CustomPalette;
+}
 
 export function getLightTheme(): Theme {
-  return createMuiTheme({
+  return createMuiTheme(({
     palette: {
+      colors: {
+        // headerHighlight: '#502df1',
+        active: {
+          // highlight: '#5532f1',
+          highlight: '#674bf2',
+          hover: '#E8E5F4',
+          // background: '#b5aaf5', // left border highlight
+        },
+      },
       text: {
         primary: '#686868',
         secondary: '#686868',
@@ -14,20 +44,31 @@ export function getLightTheme(): Theme {
         light: '#62bbfc',
         main: '#0AA3F5',
         dark: '#2976b0',
-        contrastText: '#6B8E9B',
+        // contrastText: '#6B8E9B',
+        contrastText: '#FFF',
       },
       secondary: {
-        light: '#817AD5',
+        light: '#8B77F4',
         main: '#6F55F2',
-        dark: '#1B2674',
+        dark: '#4D3BA9',
 
         // color: '#7450f5',
         // color: '#6F55F2',
         // color: '#887AF5',
         // color: '#674BF2',
         // color: '#8B78F5',
+
+        // background: '#e8e6f3',
+        // background: '#E7E6F4',
+        // background: '#e7e5f2',
+        // background: '#4106f1',
+        // background: '#3c06ed',
+        // background: '#502df1',
+
+        // background: '#5532f1', // this is nice
+
         contrastText: '#FFF',
       },
     },
-  });
+  } as unknown) as AppThemeOptions);
 }
