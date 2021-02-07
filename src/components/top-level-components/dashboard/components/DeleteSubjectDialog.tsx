e9aeb205e93d494bd966882a3b8f7454758df5ec
joe-afternightfall <joe@afternightfall.com>
2021-02-07 16:06:28 -0600

    moving

--- src/components/top-level-components/dashboard/components/DeleteSubjectDialog.tsx
+++ src/components/top-level-components/dashboard/components/DeleteSubjectDialog.tsx
@@ -1,10 +1,10 @@
-import { Grid, IconButton, Typography } from '@material-ui/core';
 import React from 'react';
 import { connect } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 import { AnyAction, Dispatch } from 'redux';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { State } from '../../../../configs/redux/store';
+import { Grid, IconButton, Typography } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import { displayAppDialog } from '../../../../creators/application/app-dialog';
 import { deleteSubject } from '../../../../services/subject-list/delete-subject';
