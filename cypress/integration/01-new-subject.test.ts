c96e854f2470fd47717e5826edc04495dd09bb77
joe-afternightfall <joe@afternightfall.com>
2021-02-07 22:45:48 -0600

    adding cypress edit

--- cypress/integration/01-new-subject.test.ts
+++ cypress/integration/01-new-subject.test.ts
@@ -5,7 +5,6 @@ describe('New Subject Test', () => {
   const localBaseUrl = 'http://localhost:3000';
 
   it('should create new test subject', () => {
-    // cy.open()
     cy.visit(localBaseUrl);
     cy.get('[data-testid="add-new-subject"]').click();
     cy.get('[data-testid="subject-name"]').type('Test Subject');
@@ -14,10 +13,5 @@ describe('New Subject Test', () => {
     cy.get('[data-testid="Deep Orange"]').click();
     cy.get('[data-testid="subject-builder-save-button"]').should('be.enabled');
     cy.get('[data-testid="subject-builder-save-button"]').click();
-
-    cy.get('[data-testid="edit-Test Subject"]').click();
-    // cy.get('[title="Add Bookmark"]').click();
-    // cy.get('[data-testid="bookmark-title"]').type('My Test Bookmark');
-    // cy.get('[data-testid="subject-dropdown"]').click();
   })
 })
--- cypress/integration/02-duplicate-subject.test.ts
+++ cypress/integration/02-duplicate-subject.test.ts
@@ -4,19 +4,10 @@
 describe('Duplicate Subject Test', () => {
   const localBaseUrl = 'http://localhost:3000';
 
-  it('block user from saving when duplicate exists', () => {
-    // cy.open()
+  it('should block user from saving when duplicate exists', () => {
     cy.visit(localBaseUrl);
     cy.get('[data-testid="add-new-subject"]').click();
     cy.get('[data-testid="subject-name"]').type('Test Subject');
     cy.get('[data-testid="subject-builder-save-button"]').should('be.disabled');
-
-    // cy.get('[data-testid="Object"]').click();
-    // cy.get('[data-testid="Deep Orange"]').should('be.disabled');
-
-    // cy.get('[data-testid="edit-Test Subject"]').click();
-    // cy.get('[title="Add Bookmark"]').click();
-    // cy.get('[data-testid="bookmark-title"]').type('My Test Bookmark');
-    // cy.get('[data-testid="subject-dropdown"]').click();
   })
 })
