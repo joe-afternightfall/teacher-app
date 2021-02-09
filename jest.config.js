8488bef1764ff1091145568a0cbd9da21d63752c
joe-afternightfall <joe@afternightfall.com>
2021-02-08 22:08:00 -0600

    changing jest config

--- jest.config.js
+++ jest.config.js
@@ -34,4 +34,8 @@ module.exports = {
         "\\.tsx$": "ts-jest"
     },
     verbose: false,
+    setupFilesAfterEnv: [
+        "jest-extended",
+        "<rootDir>/src/configs/test-utils/setup-tests.ts"
+    ],
 };
