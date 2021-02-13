56715ee9fa0d9b41a361f473f9bec30c9587e430
joe-afternightfall <joe@afternightfall.com>
2021-02-12 22:57:56 -0600

    lint

--- src/creators/lesson-planner/move-items.ts
+++ src/creators/lesson-planner/move-items.ts
@@ -3,9 +3,7 @@ import { LessonWeekdays } from '../../configs/types/LessonPlanner';
 import { AnyAction } from 'redux';
 
 // todo:  consolidate similar to builder
-export const movePlannerItems = (
-  days: LessonWeekdays,
-): AnyAction => {
+export const movePlannerItems = (days: LessonWeekdays): AnyAction => {
   return {
     type: actions.MOVE_PLANNER_ITEMS,
     days: days,
