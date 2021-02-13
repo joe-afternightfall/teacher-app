b9a5349c3a597342a3f22e8f1e7888205c818698
joe-afternightfall <joe@afternightfall.com>
2021-02-12 20:02:26 -0600

    adding drag and drop creators

---
+++ src/creators/template-builder/drag-and-drop.ts
@@ -0,0 +1,22 @@
+import actions from '../actions';
+import { AnyAction } from 'redux';
+import { LessonItem } from '../../configs/models/LessonItem';
+import { LessonWeekdays } from '../../configs/types/LessonPlanner';
+
+export const reorderTemplateBuilder = (
+  items: LessonItem[],
+  dayOfWeek: string,
+): AnyAction => {
+  return {
+    type: actions.REORDER_TEMPLATE_BUILDER,
+    items: items,
+    dayOfWeek: dayOfWeek,
+  };
+}
+
+export const moveTemplateItem = (days: LessonWeekdays) => {
+  return {
+    type: actions.MOVE_TEMPLATE_ITEMS,
+    days: days,
+  }
+}
