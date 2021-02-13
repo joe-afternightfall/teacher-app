046f13a2b27710193296e82e6ec89bcabbd55f7e
joe-afternightfall <joe@afternightfall.com>
2021-02-12 21:30:44 -0600

    delting

--- src/creators/template-builder/lesson-saved.ts
+++
@@ -1,8 +0,0 @@
-import actions from '../actions';
-import { AnyAction } from 'redux';
-
-export const savedLessonTemplate = (): AnyAction => {
-  return {
-    type: actions.SAVED_LESSON_TEMPLATE,
-  };
-};
