f177671affdb90bf191c86edd822f8c905b11118
joe-afternightfall <joe@afternightfall.com>
2021-02-12 21:08:13 -0600

    deleting

--- src/creators/lesson-planner/lesson-type.ts
+++
@@ -1,9 +0,0 @@
-import actions from '../actions';
-import { AnyAction } from 'redux';
-
-export const updateLessonType = (lessonType: string | undefined): AnyAction => {
-  return {
-    type: actions.UPDATE_ITEM_TYPE,
-    lessonType: lessonType,
-  };
-};
