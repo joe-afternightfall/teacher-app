1d23301edd83abc8c7e58b673941c9b94fc1b652
joe-afternightfall <joe@afternightfall.com>
2021-02-16 08:49:40 -0600

    adding to test coverage

--- src/creators/bookmarks/bookmarks.test.ts
+++ src/creators/bookmarks/bookmarks.test.ts
@@ -1,6 +1,6 @@
 import actions from '../actions';
 import { buildBookmarkList } from '../../configs/test-utils/test-util';
-import { loadBookmarksList } from './bookmarks';
+import { loadBookmarksList, updateBookmarkSubject, updateBookmarkTitle, updateBookmarkUrl } from './bookmarks';
 
 describe('bookmarks creators', () => {
   it('should return LOAD_BOOKMARKS_LIST action', () => {
@@ -11,5 +11,32 @@ describe('bookmarks creators', () => {
       type: actions.LOAD_BOOKMARKS_LIST,
       bookmarks: bookmarks,
     });
+  });
+
+  it('should return UPDATE_BOOKMARK_SUBJECT_ID', () => {
+    const response = updateBookmarkSubject('test-subject');
+
+    expect(response).toEqual({
+      type: actions.UPDATE_BOOKMARK_SUBJECT_ID,
+      value: 'test-subject',
+    })
+  });
+
+  it('should return UPDATE_BOOKMARK_URL', () => {
+    const response = updateBookmarkUrl('test-url');
+
+    expect(response).toEqual({
+      type: actions.UPDATE_BOOKMARK_URL,
+      value: 'test-url',
+    })
+  });
+
+  it('should return UPDATE_BOOKMARK_TITLE', () => {
+    const response = updateBookmarkTitle('test-title');
+
+    expect(response).toEqual({
+      type: actions.UPDATE_BOOKMARK_TITLE,
+      value: 'test-title',
+    })
   });
 });
