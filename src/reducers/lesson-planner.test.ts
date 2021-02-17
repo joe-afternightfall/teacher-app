b523d59a4d2bc2eb17ee91e34032eae907fd3916
joe-afternightfall <joe@afternightfall.com>
2021-02-17 08:40:54 -0600

    adding test coverage

--- src/reducers/lesson-planner.test.ts
+++ src/reducers/lesson-planner.test.ts
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import actions from '../creators/actions';
 import lessonPlanner from './lesson-planner';
 import { buildLessonPlanner } from '../configs/test-utils/test-util';
+import { getStore } from '../configs/test-utils/mock-redux';
 
 describe('Weekly Planner State', () => {
   it('should load weekly planners', () => {
@@ -15,6 +16,20 @@ describe('Weekly Planner State', () => {
     expect(state.lessonPlanners).toBe(lessonPlanners);
     expect(state.selectedLessonId).toBe('planner-id');
   });
+
+  // it('should return SELECT_LESSON_BY_ID action', () => {
+  //   const id = uuidv4();
+  //   const appState = getStore({}).getState();
+  //
+  //   const state = lessonPlanner.reducer(appState, {
+  //     type: actions.SELECT_LESSON_BY_ID,
+  //     id: 'planner-id',
+  //   });
+  //
+  //   expect(state).toEqual({
+  //     selectedLessonId: id,
+  //   });
+  // });
 
   it('should return UPDATE_LESSON_CONTENT action', () => {
     const content = uuidv4();
@@ -50,6 +65,70 @@ describe('Weekly Planner State', () => {
     });
 
     expect(state.lessonBoardChanged).toEqual(false);
+  });
+
+  it('should return UPDATE_WEEK_NUMBER action', () => {
+    const weekNumber = uuidv4();
+
+    const state = lessonPlanner.reducer(undefined, {
+      type: actions.UPDATE_WEEK_NUMBER,
+      weekNumber: weekNumber
+    });
+
+    expect(state).toEqual({
+      weekNumber: weekNumber,
+    });
+  });
+
+  it('should return UPDATE_PLANNER_TITLE action', () => {
+    const title = uuidv4();
+
+    const state = lessonPlanner.reducer(undefined, {
+      type: actions.UPDATE_PLANNER_TITLE,
+      plannerTitle: title
+    });
+
+    expect(state).toEqual({
+      plannerTitle: title,
+    });
+  });
+
+  it('should return UPDATE_PLANNER_START_DATE action', () => {
+    const date = new Date().toLocaleDateString();
+
+    const state = lessonPlanner.reducer(undefined, {
+      type: actions.UPDATE_PLANNER_START_DATE,
+      startDate: date
+    });
+
+    expect(state).toEqual({
+      plannerStartDate: date,
+    });
+  });
+
+  it('should return UPDATE_PLANNER_END_DATE action', () => {
+    const date = new Date().toLocaleDateString();
+
+    const state = lessonPlanner.reducer(undefined, {
+      type: actions.UPDATE_PLANNER_END_DATE,
+      endDate: date
+    });
+
+    expect(state).toEqual({
+      plannerEndDate: date,
+    });
+  });
+
+  it('should return CLEAR_NEW_PLANNER_INFO action', () => {
+    const state = lessonPlanner.reducer(undefined, {
+      type: actions.CLEAR_NEW_PLANNER_INFO,
+    });
+
+    expect(state).toEqual({
+      weekNumber: '',
+      plannerStartDate: new Date().toLocaleDateString(),
+      plannerEndDate: new Date().toLocaleDateString()
+    });
   });
 
   it('should return empty object', () => {
--- src/reducers/subject-list.test.ts
+++ src/reducers/subject-list.test.ts
@@ -1,10 +1,16 @@
 import { v4 as uuidv4 } from 'uuid';
 import actions from '../creators/actions';
-import subjectList from '../reducers/subject-list';
+import subjectList, { SubjectListState } from '../reducers/subject-list';
 import { buildColor, buildSubjectList } from '../configs/test-utils/test-util';
+import { getStore } from '../configs/test-utils/mock-redux';
+import { checkForDuplicates } from '../utils/validate-name';
+
+jest.mock('../utils/validate-name');
+
+const checkForDuplicatesMock = checkForDuplicates as jest.Mock;
 
 describe('subject list reducer', () => {
-  it('should return action', () => {
+  it('should return INITIALIZE action', () => {
     const subjects = buildSubjectList(2);
     const state = subjectList.reducer(undefined, {
       type: actions.INITIALIZE,
@@ -36,30 +42,46 @@ describe('subject list reducer', () => {
     expect(state.selectedIconId).toEqual(id);
   });
 
-  it('should return LOAD_SUBJECT_LIST action', () => {
-    const subjects = buildSubjectList(4);
+  it('should return UPDATE_SUBJECT_NAME when duplicate true', () => {
+    const subjectName = uuidv4();
 
-    const state = subjectList.reducer(undefined, {
-      type: actions.LOAD_SUBJECT_LIST,
-      subjectList: subjects,
+    checkForDuplicatesMock.mockReturnValue(true);
+    const state = subjectList.reducer(buildSubjectListState(), {
+      type: actions.UPDATE_SUBJECT_NAME,
+      subjectName: subjectName,
     });
 
-    expect(state.subjectList).toEqual(subjects);
+    expect(state.subjectName).toEqual(subjectName);
+    expect(state.subjectNameError).toEqual(true);
   });
 
-  it('should return CLEAR_SUBJECT_INFO_DIALOG action', () => {
-    const state = subjectList.reducer(undefined, {
-      type: actions.CLEAR_SUBJECT_BUILDER_DIALOG,
+  it('should return UPDATE_SUBJECT_NAME when duplicate false', () => {
+    const subjectName = uuidv4();
+
+    checkForDuplicatesMock.mockReturnValue(false);
+    const state = subjectList.reducer(buildSubjectListState(), {
+      type: actions.UPDATE_SUBJECT_NAME,
+      subjectName: subjectName,
     });
 
-    expect(state.subjectName).toEqual('');
-    expect(state.selectedIconId).toEqual('');
-    expect(state.selectedColor).toEqual({
-      id: '',
-      name: '',
-      primaryColor: '',
-      secondaryColor: '',
+    expect(state.subjectName).toEqual(subjectName);
+    expect(state.subjectNameError).toEqual(false);
+  });
+
+  it('should return UPDATE_SUBJECT_NAME when editing form true', () => {
+    const subjectName = uuidv4();
+
+    checkForDuplicatesMock.mockReturnValue(true);
+    const appState = buildSubjectListState();
+    appState.editingForm = true;
+
+    const state = subjectList.reducer(appState, {
+      type: actions.UPDATE_SUBJECT_NAME,
+      subjectName: subjectName,
     });
+
+    expect(state.subjectName).toEqual(subjectName);
+    expect(state.subjectNameError).toEqual(false);
   });
 
   it('should return true for display subject info', () => {
@@ -94,6 +116,46 @@ describe('subject list reducer', () => {
     expect(state.displayLoader).toEqual(false);
   });
 
+  it('should return LOAD_SUBJECT_LIST action', () => {
+    const subjects = buildSubjectList(4);
+
+    const state = subjectList.reducer(undefined, {
+      type: actions.LOAD_SUBJECT_LIST,
+      subjectList: subjects,
+    });
+
+    expect(state.subjectList).toEqual(subjects);
+  });
+
+  it('should return CLEAR_SUBJECT_INFO_DIALOG action', () => {
+    const state = subjectList.reducer(undefined, {
+      type: actions.CLEAR_SUBJECT_BUILDER_DIALOG,
+    });
+
+    expect(state.subjectName).toEqual('');
+    expect(state.selectedIconId).toEqual('');
+    expect(state.selectedColor).toEqual({
+      id: '',
+      name: '',
+      primaryColor: '',
+      secondaryColor: '',
+    });
+  });
+
+  it('should return EDITING_SUBJECT action', () => {
+    const state = subjectList.reducer(buildSubjectListState(), {
+      type: actions.EDITING_SUBJECT,
+      subjectId: 'id-3'
+    });
+
+    expect(state.editingForm).toEqual(true);
+    expect(state.subjectName).toEqual('subject-name-3');
+    expect(state.selectedIconId).toBeDefined();
+    expect(state.editingFormFirebaseId).toBeDefined();
+    expect(state.editingFormId).toBeDefined();
+    expect(state.selectedColor).toBeDefined();
+  });
+
   it('should return false for editing form', () => {
     const state = subjectList.reducer(undefined, {
       type: actions.CLEAR_EDITING,
@@ -110,3 +172,19 @@ describe('subject list reducer', () => {
     expect(state).toEqual({});
   });
 });
+
+function buildSubjectListState(): SubjectListState {
+  return {
+    subjectList: buildSubjectList(4),
+    editingForm: false,
+    subjectName: 'subject-name',
+    selectedIconId: 'icon-id',
+    selectedColor: buildColor(1)[0],
+    displaySubjectBuilder: false,
+    displayLoader: false,
+    editingFormFirebaseId: 'firebase-edit-id',
+    editingFormId: 'edit-id',
+    subjectNameError: false,
+    ableToSubmit: true
+  };
+}
