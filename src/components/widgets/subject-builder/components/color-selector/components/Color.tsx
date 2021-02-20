110f1e66fd5349de1d1fa649699fd718482ab13b
joe-afternightfall <joe@afternightfall.com>
2021-02-20 12:00:05 -0600

    extracting out color component

---
+++ src/components/widgets/subject-builder/components/color-selector/components/Color.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import CheckIcon from '@material-ui/icons/Check';
+
+export default function Color(props: ColorProps): JSX.Element {
+  return (
+    <Grid
+      container
+      justify={'center'}
+      alignItems={'center'}
+      style={{ height: '100%' }}
+    >
+      <CheckIcon style={{ margin: 'auto', color: props.color }} />
+    </Grid>
+  );
+}
+
+export interface ColorProps {
+  color: string;
+}
