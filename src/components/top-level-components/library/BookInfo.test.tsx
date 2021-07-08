9c977cb7b8074615931f502312df9cd178003d06
joe-afternightfall <joe@afternightfall.com>
2021-07-07 19:50:13 -0500

    adding book info test

---
+++ src/components/top-level-components/library/BookInfo.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import BookInfo from './BookInfo';
+import { render } from '@testing-library/react';
+import { chance, getChance } from 'jest-chance';
+
+describe('Book Info Component', () => {
+  it('should render 2 text fields', () => {
+
+    let title = chance.string();
+    let author = chance.string();
+
+    const info = {
+      firebaseId: chance.string(),
+    id: chance.string(),
+    title: title,
+    author: author,
+    genre: chance.string(),
+    gradeLevel: chance.integer(),
+    pages: chance.integer(),
+    isbn: chance.string(),
+  };
+
+    const bookInfo = render(
+      <BookInfo
+        info={info}
+        fieldsToRender={['title', 'author']}
+        blurHandler={jest.fn()}
+        changeHandler={jest.fn()}
+      />
+      );
+
+    const titleInput = bookInfo.getByDisplayValue(title) as HTMLInputElement;
+    const authorInput = bookInfo.getByDisplayValue(author) as HTMLInputElement;
+    // const input = bookInfo.getByTestId('book-title') as HTMLInputElement;
+
+    expect(titleInput.value).toBe(title);
+    expect(authorInput.value).toBe(author);
+    expect(bookInfo.container.children.length).toEqual(2);
+  });
+});
