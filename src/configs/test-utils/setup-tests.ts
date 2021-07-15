c415c1ba9dba0875295c97431eec0dcd9059cce9
joe-afternightfall <joe@afternightfall.com>
2021-07-14 22:43:40 -0500

    adding jest-canvas-mock

--- src/configs/test-utils/setup-tests.ts
+++ src/configs/test-utils/setup-tests.ts
@@ -5,3 +5,4 @@ require('jest-fetch-mock').enableMocks();
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import 'jest-canvas-mock';
